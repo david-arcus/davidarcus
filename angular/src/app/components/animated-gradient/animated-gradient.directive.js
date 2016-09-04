e4bd3eccb3e8629edad741e654875a3ce6f242dc
David Arcus <arcus.david@gmail.com>
2016-09-04 16:46:49 +1200

    - update noise texture and encode it as base64 - try to implement delays between transitions - put some buttons to talk to directive

--- angular/src/app/components/animated-gradient/animated-gradient.directive.js
+++ angular/src/app/components/animated-gradient/animated-gradient.directive.js
@@ -28,26 +28,35 @@
       var second = background[1];
       
       var loveCouple = ['#3a6186', '#89253e'];
-      
       var sweetMorning = ['#FF5F6D', '#FFC371'];
-      
       var transfile = ['#16BFFD', '#CB3066'];
-      
-      var greenAndBlue = ['#c2e59c', '#64b3f4'];    
+      var greenAndBlue = ['#c2e59c', '#64b3f4'];
+      var home = ['#28e59a', '#36dee0'];
 
       var tl = new TimelineMax({repeat:-1,yoyo:true});
-      
-      tl.to( first, 10, { stopColor:loveCouple[0] }, 'loveCouple');
-      tl.to( second, 10, { stopColor:loveCouple[1] }, 'loveCouple');
       
       tl.to( first, 10, { stopColor:sweetMorning[0] }, 'sweetMorning');
       tl.to( second, 10, { stopColor:sweetMorning[1] }, 'sweetMorning');
       
+      tl.delay(10);
+      
+      tl.to( first, 10, { stopColor:loveCouple[0] }, 'loveCouple');
+      tl.to( second, 10, { stopColor:loveCouple[1] }, 'loveCouple');
+      
+      tl.delay(10);
+      
       tl.to( first, 10, { stopColor:transfile[0] }, 'transfile');
       tl.to( second, 10, { stopColor:transfile[1] }, 'transfile');
       
+      tl.delay(10);
+      
       tl.to( first, 10, { stopColor:greenAndBlue[0] }, 'greenAndBlue');
       tl.to( second, 10, { stopColor:greenAndBlue[1] }, 'greenAndBlue');
+      
+      tl.delay(10);
+      
+      tl.to( first, 10, { stopColor:home[0] }, 'home');
+      tl.to( second, 10, { stopColor:home[1] }, 'home');
       
       
       
--- angular/src/app/main/main.controller.js
+++ angular/src/app/main/main.controller.js
@@ -6,12 +6,19 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController() {
+  function MainController($log) {
     var vm = this;
 
     vm.awesomeThings = [];
     vm.classAnimation = '';
     vm.creationDate = 1463809710965;
+    
+    vm.changeGradient = function(gradientType) {
+      
+      $log.debug('hello');
+      $log.debug(gradientType);
+      
+    }
     
   }
 })();
